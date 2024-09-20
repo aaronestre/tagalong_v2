41a9bb48d6dbc795599108613c1f29be3faefafe
G8t0rboi <39623865+G8t0rboi@users.noreply.github.com>
2024-09-20 14:06:10 -0400

    Updated AppShell Navbar to be more cohesive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { AppShell, NavLink } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
-import { IconSchool } from '@tabler/icons-react';
-import { IconBook2 } from '@tabler/icons-react';
+import { IconSchool } from "@tabler/icons-react";
+import { IconBook2 } from "@tabler/icons-react";
 
 function Navbar() {
   const navigate = useNavigate();
 
   return (
     <AppShell.Navbar p="md" style={{ gap: "10px" }}>
-      <NavLink
-        leftSection={<IconSchool />}
-        label="Tutor"
-        onClick={() => navigate("/chat")}
-        style={{ margin: "5px" }}
-      />
-      <NavLink
-        leftSection={<IconBook2 />}
-        label="Vocab"
-        onClick={() => navigate("/vocab")}
-        style={{ margin: "5px" }}
-      />
-      <div style={{ flexGrow: 1 }}></div>
-      <NavLink label="Logout" />
+      <AppShell.Section>
+        <NavLink
+          leftSection={<IconSchool />}
+          label="Tutor"
+          onClick={() => navigate("/chat")}
+          style={{ margin: "5px" }}
+        />
+        <NavLink
+          leftSection={<IconBook2 />}
+          label="Vocab"
+          onClick={() => navigate("/vocab")}
+          style={{ margin: "5px" }}
+        />
+      </AppShell.Section>
+      <AppShell.Section grow></AppShell.Section>
+      <AppShell.Section>
+        <NavLink label="Logout" />
+      </AppShell.Section>
     </AppShell.Navbar>
   );
 }
